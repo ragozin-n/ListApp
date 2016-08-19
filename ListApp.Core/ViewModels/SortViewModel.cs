213a9151603e0586191299a4d52c6a9e9054a401
Kostya174 <kostya.74.74@gmail.com>
2016-08-19 05:17:10 +0500

    Экран сортировки
    Только вёрстка

---
+++ ListApp.Core/ViewModels/SortViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using MvvmCross.Core.ViewModels;
+
+namespace ListApp.Core
+{
+    public class SortViewModel : MvxViewModel
+    {
+        private List<string> _sortList;
+
+        public List<string> SortList
+        {
+            get { return _sortList; }
+            set
+            {
+                _sortList = value;
+                RaisePropertyChanged(() => SortList);
+            }
+        }
+    }
+}
