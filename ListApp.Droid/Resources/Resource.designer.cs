ec3f3cfe92a7478271372c894f23717d0f0ed06d
hallsands <casshallsands@gmail.com>
2016-08-22 03:08:11 +0500

    unused pics deleted

--- ListApp.Droid/Resources/Resource.designer.cs
+++ ListApp.Droid/Resources/Resource.designer.cs
@@ -132,49 +132,43 @@ namespace ListApp
 			public const int important = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int kek = 2130837512;
+			public const int logo = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int logo = 2130837513;
+			public const int logout = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int logout = 2130837514;
+			public const int logout_button = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int logout_button = 2130837515;
+			public const int menu = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int menu = 2130837516;
+			public const int password = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int password = 2130837517;
+			public const int people = 2130837517;
 			
 			// aapt resource value: 0x7f02000e
-			public const int people = 2130837518;
+			public const int status = 2130837518;
 			
 			// aapt resource value: 0x7f02000f
-			public const int pig = 2130837519;
+			public const int status_done = 2130837519;
 			
 			// aapt resource value: 0x7f020010
-			public const int status = 2130837520;
+			public const int status_done_button = 2130837520;
 			
 			// aapt resource value: 0x7f020011
-			public const int status_done = 2130837521;
+			public const int status_important = 2130837521;
 			
 			// aapt resource value: 0x7f020012
-			public const int status_done_button = 2130837522;
+			public const int time = 2130837522;
 			
 			// aapt resource value: 0x7f020013
-			public const int status_important = 2130837523;
+			public const int username = 2130837523;
 			
 			// aapt resource value: 0x7f020014
-			public const int time = 2130837524;
-			
-			// aapt resource value: 0x7f020015
-			public const int username = 2130837525;
-			
-			// aapt resource value: 0x7f020016
-			public const int violet_button = 2130837526;
+			public const int violet_button = 2130837524;
 			
 			static Drawable()
 			{
