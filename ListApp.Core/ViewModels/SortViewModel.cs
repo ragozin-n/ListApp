2f4cc514d70e49504a4d28658a1436e1ecb2967a
Kostya174 <kostya.74.74@gmail.com>
2016-08-23 12:24:09 +0500

    little fix

--- ListApp.Core/ViewModels/SortViewModel.cs
+++ ListApp.Core/ViewModels/SortViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace ListApp.Core
+﻿using System.Collections.Generic;
+using MvvmCross.Core.ViewModels;
+
+namespace ListApp.Core
 {
-    public class SortViewModel
+    public class SortViewModel : MvxViewModel
     {
-         
+        private List<string> _sortList;
+
+        public List<string> SortList
+        {
+            get { return _sortList; }
+            set
+            {
+                _sortList = value;
+                RaisePropertyChanged(() => SortList);
+            }
+        }
     }
 }
