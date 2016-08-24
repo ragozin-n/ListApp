132038f99e12dc85380e498e9240c1fe722ba261
BallOfDestruction <theworldtocollapse@mail.ru>
2016-08-24 12:30:33 +0500

    TaskContainer

---
+++ ListApp.Core/TaskСontainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListApp.Core
+{
+	public class TaskСontainer
+	{
+		public TaskСontainer() { }
+		private static List<Task> _allTask = new List<Task>();
+		public static List<Task> AllTask { get { return _allTask; } }
+
+		public static void AddTask(Task task)
+		{
+			_allTask.Add(task);
+		}
+	}
+}
+
