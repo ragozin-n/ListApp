0bf6c7f204e7abf9942d6c48131d83278a7ec774
Ragozin Nikita <lastjanuary@yandex.ru>
2016-08-28 00:16:38 +0500

    тык

---
+++ ListApp.Droid/Views/ItemTaskView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.App;
+using MvvmCross.Droid.Views;
+using ListApp.Core;
+
+namespace ListApp
+{
+	public class ItemTaskView : MvxActivity
+	{
+		public new ItemTaskView ViewModel
+		{
+			get { return (ItemTaskView)base.ViewModel; }
+			set { base.ViewModel = (MvvmCross.Core.ViewModels.IMvxViewModel)value; }
+		}
+
+		protected override void OnViewModelSet()
+		{
+			SetContentView(Resource.Layout.ItemTask);
+		}
+	}
+}
+
